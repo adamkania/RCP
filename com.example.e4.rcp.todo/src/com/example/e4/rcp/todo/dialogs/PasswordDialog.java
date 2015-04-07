1b42e9e6786a2a0436beededa41998c8266b6272
ask <ask@10.10.29.90>
2015-04-07 20:32:31 +0200

    Dialog

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/dialogs/PasswordDialog.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/dialogs/PasswordDialog.java
@@ -2,7 +2,6 @@ package com.example.e4.rcp.todo.dialogs;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.window.IShellProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
@@ -15,7 +14,7 @@ import org.eclipse.swt.widgets.Text;
 
 public class PasswordDialog extends Dialog {
 
-	protected PasswordDialog(Shell parentShell) {
+	public PasswordDialog(Shell parentShell) {
 		super(parentShell);
 	}
 
@@ -23,73 +22,71 @@ public class PasswordDialog extends Dialog {
 	private Text txtPassword;
 	private String user = "";
 	private String password = "";
-	
+
 	@Override
-	protected Control createDialogArea(Composite parent){
+	protected Control createDialogArea(Composite parent) {
 		Composite container = (Composite) super.createDialogArea(parent);
 		GridLayout layout = new GridLayout(2, false);
 		layout.marginRight = 5;
 		layout.marginLeft = 10;
 		container.setLayout(layout);
-		
+
 		Label lblUser = new Label(container, SWT.NONE);
-		
+
 		lblUser.setText("User:");
-		
+
 		txtUser = new Text(container, SWT.BORDER);
-		txtUser.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		txtUser.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
 		txtUser.setText(getUser());
-		
+
 		Label lblPassword = new Label(container, SWT.NONE);
-		lblPassword.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblPassword.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
+				false, 1, 1));
 		lblPassword.setText("Password");
-		
+
 		txtPassword = new Text(container, SWT.BORDER);
-		txtPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		txtPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+				false, 1, 1));
 		txtPassword.setText(getPassword());
-		
+
 		return container;
 	}
-	
-	
+
 	@Override
-	protected void createButtonsForButtonBar(Composite parent){
+	protected void createButtonsForButtonBar(Composite parent) {
 		createButton(parent, IDialogConstants.OK_ID, "Login", true);
-		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-	}
-	
-	@Override
-	protected Point getInitialSize(){
-		return new Point(450, 300);
-	}
-	
-	@Override
-	protected void okPressed(){
-		setUser(txtUser.getText());
-		setPassword(txtPassword.getText());
-		
-		super.okPressed();
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.CANCEL_LABEL, false);
 	}
 
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 300);
+	}
+
+	@Override
+	protected void okPressed() {
+		setUser(txtUser.getText());
+		setPassword(txtPassword.getText());
+
+		super.okPressed();
+	}
 
 	public String getUser() {
 		return user;
 	}
 
-
 	public void setUser(String user) {
 		this.user = user;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
 }
